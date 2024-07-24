c97a90e2d37af75b9bbeb253e95cf9dcb12993eb
CristianEduardMihai <cristianeduardmihai@gmail.com>
2024-07-24 22:37:32 +0300

    Bump User-Agent

--- api.py
+++ api.py
@@ -29,7 +29,7 @@ def fetch_user_data():
         # Specify additional headers to avoid 406 error
         request_headers = {
             "Accept": "application/json",
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"
         }
         response = requests.get(DUOLINGO_URL, headers=request_headers)
         response.raise_for_status()
